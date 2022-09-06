847d7838917baf54e5ed4e70b2fc7c02dd57e549
Audrey Macharia <mitchelaudrey@gmail.com>
2022-09-06 17:00:09 +0300

    update new files

--- src/components/tasks/TaskList.vue
+++ src/components/tasks/TaskList.vue
@@ -23,11 +23,21 @@ let showEditModal = ref(false)
 function deleteTask(task){
     store.dispatch('deleteTask', task)
 }
+function select(event){
+    alert = (event.currentTarget.id);
 
+}
+
+// $(document).ready(function() {
+//     $("a").click(function(event) {
+//         // this.append wouldn't work
+//         $(this).append(" Edit Task");
+//     });
+// });
 </script>
 
 <template>
-   <h1 align='center'>Task Management Api</h1>
+   <h1 align='center'></h1>
    <button @click="showModal = true" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" data-modal-toggle="authentication-modal">
     Add Task
     </button>
@@ -41,7 +51,7 @@ function deleteTask(task){
                         <th>Date Added</th>
                         <th>Completed</th>
                         <th>Date Completed</th>
-                        <th>Action</th>
+                        <!-- <th>Action</th> -->
                     </tr>
                 <tbody>
                     <tr v-for="task in tasks" :key="task.id">
@@ -52,17 +62,18 @@ function deleteTask(task){
                         <td>{{task.deadline}}</td>
                         <td>{{task.complested}}</td>
                         <td>{{task.date_completed}}</td>
-                        <td>{{task.action}}</td>
+                        <!-- <td>{{task.action}}</td> -->
                         
 <!-- Modal toggle -->
-<button @click="showEditModal = true "  class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" :data-modal-toggle="'authentication-modal'+task.id">
+<button @click="showEditModal = true,select($event)"  class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button" :data-modal-toggle="'#modal'+task.id">
   Edit Task
 </button>
+
 <!-- <a href="#" class="btn btn-primary btn-sm" data-toggle="modal" :data-target="'#editModal'+todo.id"><i class="fa fa-pencil"></i>&nbsp Edit Task</a> &nbsp -->
                                    
 
 <!-- Main modal -->
-<div v-show="showEditModal" :id="'authentication-modal'+task.id" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
+<div v-show="showEditModal" :id="'modal'+task.id" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
     <div class="relative p-4 w-full max-w-md h-full md:h-auto">
         <!-- Modal content -->
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -107,6 +118,25 @@ function deleteTask(task){
   Delete
 </button>
 </td>
+<!-- <td>
+    <div class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+        <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                <button type="button" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" @click="$emit('close-delete')">
+                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
+                </button>
+                <div class="p-6 text-center">
+                    <img src="../../assets/img/icons8-warning-64.png" alt="warning-icon" class="mx-auto mb-4 w-14 h-14">
+                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete this Task?</h3>
+                    <button type="button" class="text-white bg-red-600 hover:bg-red-800 focus:outline-none font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                        Yes, I'm sure
+                    </button>
+                    <button type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600" @click="$emit('close-delete')">No, cancel</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</td> -->
                         
                     </tr>
                 </tbody>
--- src/router/index.js
+++ src/router/index.js
@@ -24,7 +24,20 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../components/tasks/AddTask.vue')
-    }
+    },
+
+    // {
+    //   path: '/edittask/:id',
+    //   name: 'edittask',
+    //   params:true,
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import('../compone')
+    // }
+
+
+
   ]
 })
 
