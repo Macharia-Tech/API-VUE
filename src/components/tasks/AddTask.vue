6a8960c629f0d5ed8ff8865279ee7e539ffaf2fb
Audrey Macharia <mitchelaudrey@gmail.com>
2022-09-07 17:14:23 +0300

    added date on the add task page to easen choosing of dates when adding tasks

--- src/components/tasks/AddTask.vue
+++ src/components/tasks/AddTask.vue
@@ -1,4 +1,4 @@
-<template>
+<!-- <template> -->
     <!-- <form @submit="onSubmit" class="add-form">
         <div class="form-control"> 
         <label for="text"> Title</label>
@@ -28,10 +28,10 @@
 
 
 <!-- Main modal -->
-<div id="authentication-modal" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
-    <div class="relative p-4 w-full max-w-md h-full md:h-auto">
+<!-- <div id="authentication-modal" tabindex="-1" class="overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full justify-center items-center flex" aria-modal="true" role="dialog">
+    <div class="relative p-4 w-full max-w-md h-full md:h-auto"> -->
         <!-- Modal content -->
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+        <!-- <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button type="button" @click="$emit('close')" class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white" data-modal-toggle="authentication-modal">
                 <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
                 <span class="sr-only">Close modal</span>
@@ -49,25 +49,17 @@
                     </div>
                     <div>
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Date Added</label>
-                        <input v-model="task.date_added" type="description" name="Date Added" id="Date Added" placeholder="Date Added" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
+                        <input v-model="task.date_added" type="date" name="Date Added" id="Date Added" placeholder="Date Added" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
                     </div>
                     <div>
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Deadline</label>
-                        <input v-model="task.deadline" type="description" name="Deadline" id="Deadline" placeholder="Deadline" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
+                        <input type="date" name="Deadline"  v-model="task.deadline" id="Deadline" placeholder="Deadline" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
                     </div>
                     <div>
                         <label for="description" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Date Complited</label>
-                        <input v-model="task.date_completed" type="description" name="Date Complited" id="Date Complited" placeholder="Date Complited" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
+                        <input type="date" v-model="date_completed"   name="Date Complited" id="Date Complited" placeholder="Date Complited" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required="">
                     </div>
-                    <!-- <div class="flex justify-between">
-                        <div class="flex items-start">
-                            <div class="flex items-center h-5">
-                                <input id="remember" type="checkbox" value="" class="w-4 h-4 bg-gray-50 rounded border border-gray-300 focus:ring-3 focus:ring-blue-300 dark:bg-gray-600 dark:border-gray-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800" required="">
-                            </div>
-                            <label for="remember" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Remember me</label>
-                        </div>
-                        <a href="#" class="text-sm text-blue-700 hover:underline dark:text-blue-500">Lost Password?</a>
-                    </div> -->
+                   
                     <button type="submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Save Task</button>
                     
                 </form>
@@ -91,8 +83,8 @@ import { useStore } from 'vuex'
     title: "",
     description:"",
     deadline: new Date().toISOString().slice(0,10),
-    date_added: new Date().toISOString().slice(0,10),
-    date_completed: new Date().toISOString().slice(0,10),
+    date_added:"",
+    date_completed: "",
     completed:false
 
 })
@@ -103,4 +95,4 @@ function add() {
     // onSubmit(){}
     
 </script>
-
+ -->
